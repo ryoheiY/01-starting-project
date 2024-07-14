93ddb08f0bde34751e12441819f2a933a3d996e1
ryoheiY <momoyamashia@icloud.com>
2024-07-14 13:27:46 +0900

    コンポーネントの引数追加方法１

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,41 +1,27 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from './dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  /**
-   * 選択中User
-   */
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  @Input({required: true}) avatar! : string;
+  @Input({required: true}) name! : string;
+  @Input({required: true}) id! : string;
 
   /**
-   * get path of user image
-   * signalを使う場合はcomputedと併用する
+   * get user image path
    */
-  userImagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-
-  // /**
-  //  * get path of user image
-  //  */
-  // get userImagePath(){
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
+  get imagePath (){
+    return "assets/users/" + this.avatar;
+  }
 
   /**
    * click event for user image
    */
-  onSelectUser(){
-    //console.log("Clicked " + this.selectedUser.name);
-    const randomIndex =  Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-  }
-
+  onSelectUser() {}
 }
